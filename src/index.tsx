c8f5a0148469c62f56e4af389abc6a1ee1b4e7e6
Alex González <alejandrogsa@gmail.com>
2023-03-07 15:48:47 +0100

    add theme with dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/theme.css';
 import './index.css';
 
 const container = document.getElementById('root')!;
@@ -14,7 +15,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
